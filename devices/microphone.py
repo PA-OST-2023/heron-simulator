4ebdfbfc22ec8f0ed5b3fec73a6ef40f24c14707
Alain Keller <alain.keller@ost.ch>
2023-09-27 17:03:02 +0200

    pre shopping commit

--- devices/microphone.py
+++ devices/microphone.py
@@ -1,6 +1,10 @@
 import numpy as np
+from devices.source import Source
 
 class Microphone():
+
+    _source = None
+    _position = np.zeros((1,3))
 
     def __init__(self, position):
         self.__position = position
@@ -10,6 +14,22 @@ class Microphone():
 
     def __init__(self):
         print('ok Python')
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, position):
+        self._position = position
+
+    def set_source(self, source):
+        self._source = source
+
+    def simulate(self):
+        print(1)
+        self._source.get_sound_at_position(self._position)
+        pass
 
     def test(self):
         print('Mic Test')
--- devices/source.py
+++ devices/source.py
@@ -2,12 +2,30 @@ import numpy as np
 
 class Source():
 
+    _position = np.zeros((1,3))
     def __init__(self, config):
+        self._position = np.zeros((1,3))
         pass
 
     def __init__(self):
         pass
 
+    def add_sound(self, sound_arr):
+        pass
+
+    @property
+    def position(self):
+        return self._position
+
+    @position.setter
+    def position(self, position):
+        self._position = position
+
     def test(self):
         print('source Test')
+
+    def get_sound_at_position(self, position):
+        distance = np.linalg.norm(self._position - position)
+        print(distance)
+        pass
 
